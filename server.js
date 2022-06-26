05d05722a25716c0b4387058f4f6ae61c09d3296
vinayaviator95 <vinayaviator95@gmail.com>
2022-06-26 12:11:26 +0530

    .env file added

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
+const env = require("dotenv");
+env.config();
 const NerdleController = require("./nerdleController");
+const HealthController = require("./healthController");
 app.use(express.json());
 app.use(cors());
 app.use("/nerdle", NerdleController);
+app.use("/", HealthController);
 
-app.listen(4000, (error) => {
+app.listen(process.env.PORT || 4000, (error) => {
   if (error) throw error;
-  console.log("Server is running on port 4000");
+  console.log(`Server is running on port ${process.env.PORT || 4000}`);
 });
